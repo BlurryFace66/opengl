c33bc4584b9ee854c535eedace8514149593717b
BlurryFace66 <54356052+BlurryFace66@users.noreply.github.com>
2020-12-23 13:20:45 +0800

    整理代码

--- rainEngine/源.cpp
+++ rainEngine/源.cpp
@@ -12,13 +12,8 @@ using namespace std;
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-//GLfloat vertices[] = {
-//	//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
-//	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
-//	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
-//	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
-//	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
-//};
+#pragma region Model Data
+
 glm::vec3 cubePositions[] = {
   glm::vec3(0.0f,  0.0f,  0.0f),
   glm::vec3(2.0f,  5.0f, -15.0f),
@@ -75,14 +70,21 @@ float vertices[] = {
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
+//EBO会用上
 GLuint indices[] = { // 注意索引从0开始! 
 	0, 1, 3, // 第一个三角形
 	1, 2, 3  // 第二个三角形
 };
+#pragma endregion
 
+#pragma region Camera Declare
 //初始化照相机类
 //Camera camera(glm::vec3(0, 0, 3.0f), glm::vec3(0, 0, 0), glm::vec3(0, 1.0f, 0));  //照相机位置，照相机目标，照相机UP轴
 Camera camera(glm::vec3(0, 0, 3.0f), glm::radians(15.0f), glm::radians(180.0f), glm::vec3(0, 1.0f, 0));
+
+#pragma endregion
+
+#pragma region Input Declare
 
 float lastX;
 float lastY;
@@ -91,163 +93,158 @@ bool firstMouse = true;
 void processInput(GLFWwindow* window);
 void mouse_callback(GLFWwindow* window, double xPos, double yPos);
 
+#pragma endregion
+
+unsigned int LoadImageToGPU(const char* filename, GLint internalFormat, GLenum format, int textureSlot);
+
 int main()
 {
+	#pragma region Open A Window
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		//创建GLFW Window
+		GLFWwindow* window = glfwCreateWindow(800, 600, "rainEngine", nullptr, nullptr);
 
-	//创建GLFW Window
-	GLFWwindow* window = glfwCreateWindow(800, 600, "rainEngine", nullptr, nullptr);
+		if (window == nullptr)
+		{
+			std::cout << "Failed to create GLFW window" << std::endl;
+			glfwTerminate();  //停止glfw
+			return -1;
+		}
+		glfwMakeContextCurrent(window);
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+		glfwSetCursorPosCallback(window, mouse_callback);
 
-	if (window == nullptr)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();  //停止glfw
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	glfwSetCursorPosCallback(window, mouse_callback);
+		//Init GLEW
+		glewExperimental = GL_TRUE;
+		if (glewInit() != GLEW_OK)
+		{
+			std::cout << "Failed to initialize GLEW" << std::endl;
+			return -1;
+		}
 
-	//Init GLEW
-	glewExperimental = GL_TRUE;
-	if (glewInit() != GLEW_OK)
-	{
-		std::cout << "Failed to initialize GLEW" << std::endl;
-		return -1;
-	}
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+		//glEnable(GL_CULL_FACE);   //OPENGL会自动把正面、背面都画了，所以要手动开启背面剔除
+		//glCullFace(GL_BACK);
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glEnable(GL_DEPTH_TEST);
 
-	int width, height;
-	glfwGetFramebufferSize(window, &width, &height);
-	glViewport(0, 0, width, height);
-	//glEnable(GL_CULL_FACE);   //OPENGL会自动把正面、背面都画了，所以要手动开启背面剔除
-	//glCullFace(GL_BACK);
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	glEnable(GL_DEPTH_TEST);
+	#pragma endregion
 
-	Shader* testShader = new Shader("vertexSource.txt", "fragmentSource.txt");
+	#pragma region Init Shader Program
 
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
+		Shader* testShader = new Shader("vertexSource.vert", "fragmentSource.frag");
+	#pragma endregion
 
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	#pragma region Init and Load Models to VAO,VBO
 
-	unsigned int EBO;
-	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);//先绑定EBO然后用glBufferData把索引复制到缓冲里
+		unsigned int VAO;
+		glGenVertexArrays(1, &VAO);
+		glBindVertexArray(VAO);
 
-	// 位置属性
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
-	glEnableVertexAttribArray(0);
-	// 颜色属性
-	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	//glEnableVertexAttribArray(1);
-	// UV属性
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(2);
+		unsigned int VBO;
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	//glBindBuffer(GL_ARRAY_BUFFER, 0);
-	//glBindVertexArray(0);
+		//unsigned int EBO;
+		//glGenBuffers(1, &EBO);
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);//先绑定EBO然后用glBufferData把索引复制到缓冲里
 
-	//材质A
-	unsigned int TexBufferA;
-	glGenTextures(1, &TexBufferA);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, TexBufferA);
+		// 位置属性
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+		glEnableVertexAttribArray(0);
+		// 颜色属性
+		//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+		//glEnableVertexAttribArray(1);
+		// UV属性
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+		glEnableVertexAttribArray(2);
+	#pragma endregion
 
-	int width1, height1, nrChannel;
-	stbi_set_flip_vertically_on_load(true);   //因为这个库读出来的贴图和opengl的y轴方向是相反的
-	unsigned char *data1 = stbi_load("pic.jpg", &width1, &height1, &nrChannel, 0);
+	#pragma region Init and Load Textures
 
-	if (data1)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width1, height1, 0, GL_RGB, GL_UNSIGNED_BYTE, data1);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		cout << "load image failed";
-	}
-	stbi_image_free(data1);
+		//材质A
+		unsigned int TexBufferA;
+		TexBufferA = LoadImageToGPU("pic.jpg",GL_RGB,GL_RGB,0);
 
-	//材质B
-	unsigned int TexBufferB;
-	glGenTextures(1, &TexBufferB);
-	glActiveTexture(GL_TEXTURE3);
-	glBindTexture(GL_TEXTURE_2D, TexBufferB);
-	stbi_set_flip_vertically_on_load(true);   //因为这个库读出来的贴图和opengl的y轴方向是相反的
-	unsigned char *data2 = stbi_load("awesomeface.png", &width1, &height1, &nrChannel, 0);
+		//材质B
+		unsigned int TexBufferB;
+		TexBufferB = LoadImageToGPU("awesomeface.png", GL_RGBA, GL_RGBA, 1);
+	#pragma endregion
 
-	if (data2)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width1, height1, 0, GL_RGBA, GL_UNSIGNED_BYTE, data2);   //因为是PNG，所以这里有A的值
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		cout << "load image failed";
-	}
-	stbi_image_free(data2);
+	#pragma region Transform
 
-	//矩阵计算
-	//glm::mat4 trans=glm::mat4(1.0f);
+		//矩阵计算
+		//glm::mat4 trans=glm::mat4(1.0f);
 
-	//先缩放、再旋转，最后位移
-	//trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-	//trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));   //该版本glm接收的是弧度，所以需要glm::radians转一下
-	//trans = glm::translate(trans, glm::vec3(-1.0f, 0, 0));
-	
-	glm::mat4 modelMat = glm::mat4(1.0f);
-	modelMat = glm::rotate(modelMat, glm::radians(-55.0f), glm::vec3(1.0f, 0, 0));
-	glm::mat4 viewMat = glm::mat4(1.0f);
-	//viewMat = glm::translate(viewMat, glm::vec3(0, 0, -3.0f));
-	//viewMat = camera.GetViewMatrix();
-	glm::mat4 projMat = glm::mat4(1.0f);
-	projMat = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+		//先缩放、再旋转，最后位移
+		//trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+		//trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));   //该版本glm接收的是弧度，所以需要glm::radians转一下
+		//trans = glm::translate(trans, glm::vec3(-1.0f, 0, 0));
+	#pragma endregion
+
+	#pragma region Prepare MVP matrices
+
+		glm::mat4 modelMat = glm::mat4(1.0f);
+		//modelMat = glm::rotate(modelMat, glm::radians(-55.0f), glm::vec3(1.0f, 0, 0));
+		glm::mat4 viewMat = glm::mat4(1.0f);
+		//viewMat = glm::translate(viewMat, glm::vec3(0, 0, -3.0f));
+		//viewMat = camera.GetViewMatrix();
+		glm::mat4 projMat = glm::mat4(1.0f);
+		projMat = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+	#pragma endregion
 
 	//主循环
 	while (!glfwWindowShouldClose(window))  //在我们每次循环的开始前检查一次GLFW是否被要求退出
 	{
-		//trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-		//trans = glm::rotate(trans, (GLfloat)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		//输入处理
 		processInput(window);
 
+		//清屏
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, TexBufferA);
-		glActiveTexture(GL_TEXTURE3);
-		glBindTexture(GL_TEXTURE_2D, TexBufferB);
-		glBindVertexArray(VAO);
-		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);  这里不需要EBO了
 
 		//照相机位置变化后，viewMatrix也要变
 		viewMat = camera.GetViewMatrix();
 
 		for (GLuint i = 0; i < 10; i++)
 		{
+			//设置 model matrix
 			glm::mat4 modelMat2=glm::mat4(1.0f);
 			modelMat2 = glm::translate(modelMat2, cubePositions[i]);
 
+			//可以在这里设置MVP（因为正常游戏引擎里模型是变动的）
+
+			//设置materials的shader参数
 			testShader->use();
+
+			//设置materials的texture参数
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, TexBufferA);
+			glActiveTexture(GL_TEXTURE3);
+			glBindTexture(GL_TEXTURE_2D, TexBufferB);
+
+			//设置materials的uniform参数
 			glUniform1i(glGetUniformLocation(testShader->ID, "ourTexture1"), 0);
 			glUniform1i(glGetUniformLocation(testShader->ID, "ourTexture2"), 3);   //有0~15号槽位可以用
-			//glUniformMatrix4fv(glGetUniformLocation(testShader->ID, "transform"), 1, GL_FALSE, glm::value_ptr(trans));
 			glUniformMatrix4fv(glGetUniformLocation(testShader->ID, "modelMat"), 1, GL_FALSE, glm::value_ptr(modelMat2));
 			glUniformMatrix4fv(glGetUniformLocation(testShader->ID, "viewMat"), 1, GL_FALSE, glm::value_ptr(viewMat));
 			glUniformMatrix4fv(glGetUniformLocation(testShader->ID, "projMat"), 1, GL_FALSE, glm::value_ptr(projMat));
 
+			//设置模型
+			glBindVertexArray(VAO);
+
+			//Drawcall
 			glDrawArrays(GL_TRIANGLES, 0, 36);
-			//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		}
 		//glBindVertexArray(0);
 
@@ -304,4 +301,29 @@ void mouse_callback(GLFWwindow* window, double xPos, double yPos)
 	lastY = yPos;
 	//cout << deltaX << endl;
 	camera.ProcessMouseMovement(deltaX,deltaY);
+}
+
+unsigned int LoadImageToGPU(const char* filename, GLint internalFormat,GLenum format,int textureSlot)
+{
+	unsigned int TexBuffer;
+	glGenTextures(1, &TexBuffer);
+	glActiveTexture(GL_TEXTURE0+textureSlot);  //以0为基准加上偏移量就可以切换到对应槽位
+	glBindTexture(GL_TEXTURE_2D, TexBuffer);
+
+	int width, height, nrChannel;
+	stbi_set_flip_vertically_on_load(true);   //因为这个库读出来的贴图和opengl的y轴方向是相反的
+	unsigned char *data = stbi_load(filename, &width, &height, &nrChannel, 0);
+
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		cout << "load image failed";
+	}
+	stbi_image_free(data);
+
+	return TexBuffer;
 }
